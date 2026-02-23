e44b20e5f2b49aa40115c48c3698d26b6651f422
Silvio Lorenzana <your-email@example.com>
2026-02-23 15:37:04 -0600

    fixed address

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ function localBusinessJsonLd() {
     "@context": "https://schema.org",
     "@type": "HVACBusiness",
     name: BUSINESS.name,
-    telephone: BUSINESS.phone,
+    telephone: BUSINESS.phoneE164,
     url,
     areaServed: [
       {
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,5 @@
+import { SocketAddress } from "net";
+
 export const BUSINESS = {
   name: "GC Heating & Cooling",
   phoneDisplay: "(800) 706-4822",
@@ -6,6 +8,7 @@ export const BUSINESS = {
     "https://customer.dispatch.me/booking?account_id=37&org_id=257895",
   addressLine1: "6940 Knott Ave. Suite H",
   cityStateZip: "Buena Park, CA 90621",
+  address: "6940 Knott Ave. Suite H, Buena Park, CA 90621 ",
   licenseLabel: "License # 794228",
   trustLine: "Insured and Bonded",
   serviceRegionShort: "Serving Los Angeles & Orange County",
